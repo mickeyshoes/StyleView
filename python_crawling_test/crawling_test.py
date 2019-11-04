bf8ebda18146e7eeaf657a38a4c81655a15e913f
mickeyshoes <parody9c@gmail.com>
2019-11-05 03:17:16 +0900

    191105 selenium scroll down

--- python_crawling_test/crawling_test.py
+++ python_crawling_test/crawling_test.py
@@ -1,9 +1,14 @@
 import selenium.webdriver as webdriver
+import time
 
+#스크롤을 내린다.
+def scroll_down(webdriver):
+    webdriver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    time.sleep(3)
 
 # chrome 창을 띄우지 않고 크롤링
 options = webdriver.ChromeOptions()
-options.add_argument('headless')
+#options.add_argument('headless')
 options.add_argument('disable-gpu')
 driver = webdriver.Chrome('C:\\Users\\PSL\\Documents\\StyleView\\chromedriver.exe', options=options) # 설치경로입력
 
@@ -13,7 +18,15 @@ tag = 'hm_son7' # id
 url = 'https://www.instagram.com/' + tag # id + instargram address
 
 driver.get(url)
-totalcount = driver.find_element_by_class_name('g47SY').text
-print("총 게시물의 갯수 :", totalcount)
+# image = KL4BH 한번에 12개 가져옴
+totallist = driver.find_elements_by_class_name('g47SY')
+pictures = driver.find_elements_by_class_name('FFVAD')
+print("총 게시물의 갯수 :", totallist[0].text)
+print("팔로워 :", totallist[1].text)
+print(type(pictures))
+print(len(pictures))
+scroll_down(driver)
 
+for n in pictures:
+    print(n.get_attribute('src'))
 driver.quit()
