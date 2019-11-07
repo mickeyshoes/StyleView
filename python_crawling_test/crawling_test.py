371d4b225768f018b99c66f3b6d908a4eeaf3c79
mickeyshoes <parody9c@gmail.com>
2019-11-08 04:41:53 +0900

    다시는 보고싶지 않다 신세경.

--- python_crawling_test/crawling_test.py
+++ python_crawling_test/crawling_test.py
@@ -2,6 +2,8 @@ import selenium.webdriver as webdriver
 import time
 import urllib.request
 import os
+#from bs4 import BeautifulSoup
+
 
 #스크롤을 내린다.
 def scroll_down(webdriver):
@@ -11,16 +13,21 @@ def scroll_down(webdriver):
 
 # chrome 창을 띄우지 않고 크롤링
 options = webdriver.ChromeOptions()
-options.add_argument('headless')
+#options.add_argument('headless')
 options.add_argument('disable-gpu')
-driver = webdriver.Chrome('C:\\Users\\user\\Documents\\StyleView\\chromedriver.exe', options=options) # 설치경로입력
+driver = webdriver.Chrome('C:\\Users\\PSL\\Documents\\StyleView\\chromedriver.exe', options=options) # 설치경로입력
 
 driver.implicitly_wait(15)
 
 tag = 'sjkuksee' # id
 url = 'https://www.instagram.com/' + tag # id + instargram address
 
+ID = input('ID')
+PW = input('PW')
+
 driver.get(url)
+#soup = BeautifulSoup("""<body class ="" style="overflow: hidden;">""", "lxml")
+#soup.find("body")['style'] = 'style = "overflow: auto;"'
 # image = KL4BH 한번에 12개 가져옴
 totallist = driver.find_elements_by_class_name('g47SY')
 profile = driver.find_element_by_class_name('_6q-tv')
@@ -34,14 +41,30 @@ print(len(pictures))
 scroll_down(driver)
 
 for n in pictures:
+
+
     print(n.get_attribute('src'))'''
 
 photo_list = []
 total_count = int(totallist[0].text)
+overlap_count = 0
 if total_count > 101:
-    total_count = 100
+    total_count = 200
+
+    time.sleep(1)
+    driver.find_element_by_xpath('/html/body/span/section/nav/div[2]/div/div/div[3]/div/span/a[1]/button').click()
+    driver.find_elements_by_name('username')[0].send_keys(ID)
+    driver.find_elements_by_name('password')[0].send_keys(PW)
+    time.sleep(2)
+    driver.find_element_by_xpath('/html/body/span/section/main/div/article/div/div[1]/div/form/div[4]/button').submit()
+
 
 while True:
+
+    '''if overlap_count == 0 :
+        for n in range(3):'''
+
+
     pictures = driver.find_elements_by_class_name('FFVAD')
     print(len(pictures))
     #pic_list = []
@@ -57,23 +80,44 @@ while True:
         else:
             photo_list.append(temp)
 
+    
+
     scroll_down(driver)
+
     print(len(photo_list))
 
-    if(len(photo_list) >= total_count):
+
+    if len(photo_list) > total_count:
         break
 
-for n in photo_list:
-    print(n['src'])
+# for n in photo_list:
+#     print(n['src'])
 
 print(len(photo_list))
+# print(photo_list)
 
-save_path = 'C:\\Users\\user\\Documents\\StyleView\\python_crawling_test\\'+tag+'\\'
+tmpd = {}
+tmpl = []
+
+for n in photo_list:
+    tmpd[n['src']] = n['src']
+    
+tmpl = tmpd.values()
+
+save_path = 'C:\\Users\\PSL\\Documents\\StyleView\\python_crawling_test\\'+tag+'\\'
 
 if not os.path.isdir(save_path):
     os.makedirs(save_path)
 
-for i, n in enumerate(photo_list):
-    urllib.request.urlretrieve(n['src'], save_path + tag + str(i)+'.jpg')
+# for i, n in enumerate(photo_list):
+#     urllib.request.urlretrieve(n['src'], save_path + tag + str(i)+'.jpg')
+
+index = 0
+
+print(tmpl)
+
+for n in tmpl:
+    urllib.request.urlretrieve(n, save_path + tag + str(index)+'.jpg')
+    index+=1
 
 driver.quit()
