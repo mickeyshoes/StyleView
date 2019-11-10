2adb19f091626dbea2210037f66625ffbb0a0e8c
mickeyshoes <parody9c@gmail.com>
2019-11-11 01:32:42 +0900

    191111 google vision test 01

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # StyleView
-python 3.8
-pip 19.2.3
+python 3.8 -> 3.7.5 (확정 - google-cloud-vision install 오류 없음)
+pip 19.3.1 -> 19.2.3
+numpy 1.17.3
 django 2.2.6
 git 2.23.0
 chrome 77.0.3865.120(공식 빌드) (64bit)
--- python_crawling_test/crawling_test.py
+++ python_crawling_test/crawling_test.py
@@ -1,15 +1,13 @@
 import selenium.webdriver as webdriver
+from selenium.common.exceptions import NoSuchElementException
 import time
 import urllib.request
 import os
-#from bs4 import BeautifulSoup
-
 
 #스크롤을 내린다.
 def scroll_down(webdriver):
     webdriver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
     time.sleep(3)
-
 
 # chrome 창을 띄우지 않고 크롤링
 options = webdriver.ChromeOptions()
@@ -19,55 +17,40 @@ driver = webdriver.Chrome('C:\\Users\\PSL\\Documents\\StyleView\\chromedriver.ex
 
 driver.implicitly_wait(15)
 
-tag = 'sjkuksee' # id
+tag = 'hm_son7' # id
 url = 'https://www.instagram.com/' + tag # id + instargram address
 
 ID = input('ID')
 PW = input('PW')
 
 driver.get(url)
-#soup = BeautifulSoup("""<body class ="" style="overflow: hidden;">""", "lxml")
-#soup.find("body")['style'] = 'style = "overflow: auto;"'
-# image = KL4BH 한번에 12개 가져옴
 totallist = driver.find_elements_by_class_name('g47SY')
 profile = driver.find_element_by_class_name('_6q-tv')
 print("프로필 사진:", profile.get_attribute('src'))
 print("총 게시물의 갯수 :", totallist[0].text)
 print("팔로워 :", totallist[1].text)
 
-'''pictures = driver.find_elements_by_class_name('FFVAD')
-print(type(pictures))
-print(len(pictures))
-scroll_down(driver)
-
-for n in pictures:
-
-
-    print(n.get_attribute('src'))'''
-
 photo_list = []
 total_count = int(totallist[0].text)
-overlap_count = 0
-if total_count > 101:
-    total_count = 200
+# if total_count > 101:
+#     total_count = 100
 
-    time.sleep(1)
-    driver.find_element_by_xpath('/html/body/span/section/nav/div[2]/div/div/div[3]/div/span/a[1]/button').click()
-    driver.find_elements_by_name('username')[0].send_keys(ID)
-    driver.find_elements_by_name('password')[0].send_keys(PW)
-    time.sleep(2)
-    driver.find_element_by_xpath('/html/body/span/section/main/div/article/div/div[1]/div/form/div[4]/button').submit()
+# 크롤링 중 로그인 문제로 인한 로그인 선행
+time.sleep(1)
+driver.find_element_by_xpath('/html/body/span/section/nav/div[2]/div/div/div[3]/div/span/a[1]/button').click()
+driver.find_elements_by_name('username')[0].send_keys(ID)
+driver.find_elements_by_name('password')[0].send_keys(PW)
+time.sleep(2)
+driver.find_element_by_xpath('/html/body/span/section/main/div/article/div/div[1]/div/form/div[4]/button').submit()
 
 
+#이미지 크롤링 시작
 while True:
-
-    '''if overlap_count == 0 :
-        for n in range(3):'''
-
 
     pictures = driver.find_elements_by_class_name('FFVAD')
     print(len(pictures))
-    #pic_list = []
+    #현재의 스크롤 높이를 가져옴
+    last_height = driver.execute_script("return document.body.scrollHeight")
 
     for n in pictures:
         temp = {}
@@ -80,29 +63,24 @@ while True:
         else:
             photo_list.append(temp)
 
-    
-
     scroll_down(driver)
+
+    #스크롤을 내린 높이를 가져옴
+    scrolled_height = driver.execute_script("return document.body.scrollHeight")
 
     print(len(photo_list))
 
-
-    if len(photo_list) > total_count:
+    if scrolled_height == last_height:
         break
-
-# for n in photo_list:
-#     print(n['src'])
-
-print(len(photo_list))
-# print(photo_list)
-
-tmpd = {}
-tmpl = []
+    
+# dictionary의 key 값은 중복이 되지 않는 점을 이용하여 중복 제거
+tmp_dict = {}
+tmp_list = []
 
 for n in photo_list:
-    tmpd[n['src']] = n['src']
+    tmp_dict[n['src']] = n['src']
     
-tmpl = tmpd.values()
+tmp_list = tmp_dict.values()
 
 save_path = 'C:\\Users\\PSL\\Documents\\StyleView\\python_crawling_test\\'+tag+'\\'
 
@@ -114,10 +92,8 @@ if not os.path.isdir(save_path):
 
 index = 0
 
-print(tmpl)
-
-for n in tmpl:
-    urllib.request.urlretrieve(n, save_path + tag + str(index)+'.jpg')
+for n in tmp_list:
+    urllib.request.urlretrieve(n, save_path + tag + '_' +str(index)+'.jpg')
     index+=1
 
 driver.quit()
